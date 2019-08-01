bbf5c0a40f73e0c6a74555e91bec8e619c3547e4
jichuan <jichuandada@gmail.com>
2019-08-01 13:51:47 +0800

    eureka 集群搭建

---
+++ eureka-service3/src/main/java/com/don/eureka/EurekaService3.java
@@ -0,0 +1,23 @@
+package com.don.eureka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author jc
+ * @Description:
+ * @date 2019/7/31
+ */
+@SpringBootApplication
+@EnableEurekaServer
+@Slf4j
+public class EurekaService3 {
+
+    public static void main(String[] args) {
+        log.info("EurekaService 开始启动 -------");
+        SpringApplication.run(EurekaService3.class, args);
+        log.info("EurekaService 启动成功 -------");
+    }
+}
